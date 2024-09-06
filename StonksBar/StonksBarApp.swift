7914e99b40ed352b8c837687ed7347d2fddb27b6
Thomas Paolino <tpao24@Outlook.com>
2024-09-06 10:58:31 -0400

    Initial Commit

---
+++ StonksBar/StonksBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksBarApp.swift
+//  StonksBar
+//
+//  Created by Thomas Paolino on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StonksBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
